ad18e63a1126469615bc5d76733b04c2a4af294a
ESIC <ESIC@192.168.1.44>
2019-02-25 15:33:17 +0100

    class Note

--- src/java/bean/Note.java
+++ src/java/bean/Note.java
@@ -11,7 +11,61 @@ package bean;
  */
 public class Note {
     private int id;
-    private String text;
+    private String contenu;
     private User auteur;
     private User destinataire;
+
+    public Note(){
+        
+    }
+    public Note(int id, String text, User auteur, User destinataire) {
+        this.id = id;
+        this.contenu = text;
+        this.auteur = auteur;
+        this.destinataire = destinataire;
+    }
+
+    public Note(String text, User auteur, User destinataire) {
+        this.contenu = text;
+        this.auteur = auteur;
+        this.destinataire = destinataire;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public User getAuteur() {
+        return auteur;
+    }
+
+    public User getDestinataire() {
+        return destinataire;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setContenu(String text) {
+        this.contenu = text;
+    }
+
+    public void setAuteur(User auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setDestinataire(User destinataire) {
+        this.destinataire = destinataire;
+    }
+
+    public String infoNote() {
+        return "Note{" + "id=" + id + ", contenu=" + contenu + ", auteur=" + auteur + ", destinataire=" + destinataire + '}';
+    }
+    
+    
 }
--- src/java/dao/NoteDao.java
+++ src/java/dao/NoteDao.java
@@ -5,10 +5,24 @@
  */
 package dao;
 
+import bean.Note;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author ESIC
  */
 public class NoteDao {
     
+    public static void insert(Note note) throws SQLException{
+        String sql = "INSERT INTO note(contenu, idAuteur, idDestinataire) VALUES(?, ?, ?)";
+        /*Connection connexion = testBd.getConnection();
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        ordre.setString(1, note.getContenu());
+        ordre.setString(2, note.getAuteur().getId());
+        ordre.setString(3, note.getDestinataire().getId());
+        ordre.execute();*/
+    }
 }
